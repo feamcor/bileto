1d87780878d26e4fe688088419edc338d78c8962
Fabio Correa <feamcor@gmail.com>
2019-02-16 12:56:01 -0200

    Change solc version to 0.5.4

--- truffle-config.js
+++ truffle-config.js
@@ -8,47 +8,47 @@ const rinkebyAddressIndex = process.env.RINKEBY_ADDRESS_INDEX;
 const rinkebyNumAddresses = process.env.RINKEBY_NUM_ADDRESSES;
 
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
-    },
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        },
 
-    ganachecli: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
-    },
+        ganachecli: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        },
 
-    ganachegui: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
-    },
+        ganachegui: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
 
-    rinkeby: {
-      provider: () =>
-        new HDWalletProvider(
-          rinkebyMnemonic,
-          `https://rinkeby.infura.io/v3/${rinkebyInfuraKey}`,
-          rinkebyAddressIndex,
-          rinkebyNumAddresses
-        ),
-      network_id: 4,
-      skipDryRun: true
-    }
-  },
-
-  compilers: {
-    solc: {
-      version: "0.5.3",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200
+        rinkeby: {
+            provider: () =>
+                new HDWalletProvider(
+                    rinkebyMnemonic,
+                    `https://rinkeby.infura.io/v3/${rinkebyInfuraKey}`,
+                    rinkebyAddressIndex,
+                    rinkebyNumAddresses
+                ),
+            network_id: 4,
+            skipDryRun: true
         }
-      }
+    },
+
+    compilers: {
+        solc: {
+            version: "0.5.4",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }
     }
-  }
 };
